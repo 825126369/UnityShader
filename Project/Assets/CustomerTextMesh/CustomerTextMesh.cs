edb147c8a2b82d6e675fbf643d9985df6ca90000
xuke2020 <825126369@qq.com>
2020-06-09 12:00:46 +0800

    增加一个简单的 自定义Text，方便艺术字集成，以及 顶点动画

---
+++ Project/Assets/CustomerTextMesh/CustomerTextMesh.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class CustomerTextMesh : MonoBehaviour
+{
+    public string m_Text;
+    public Font m_Font;
+    public TextAlignment mTextAlignment;
+    public int FontSize;
+    
+    private MeshFilter mMeshFilter;
+    private MeshRenderer mMeshRenderer;
+    private Mesh mMesh;
+    
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<Vector2> uvs0 = new List<Vector2>();
+    private List<int> triangles = new List<int>();
+
+    private void Start()
+    {
+        mMeshFilter = GetComponent<MeshFilter>();
+        mMeshRenderer = GetComponent<MeshRenderer>();
+        mMesh = new Mesh();
+    }
+
+    private void Update()
+    {
+        UpdateMesh();
+        UpdateMaterial();
+    }
+
+    public string text
+    {
+        get
+        {
+            return m_Text;
+        }
+
+        set
+        {
+            if (value != m_Text)
+            {
+                m_Text = value;
+                UpdateMesh();
+            }
+        }
+    }
+
+    private float GetWidth()
+    {
+        float textWidth = 0.0f;
+        for(int i = 0; i < m_Text.Length; i++)
+        {
+            char c = m_Text[i];
+            CharacterInfo mCharacterInfo;
+            if (m_Font.GetCharacterInfo(c, out mCharacterInfo))
+            {
+                textWidth += mCharacterInfo.advance;
+            }
+        }
+        return textWidth;
+    }
+
+    private int GetValidLength()
+    {
+        int nLength = 0;
+        for (int i = 0; i < m_Text.Length; i++)
+        {
+            char c = m_Text[i];
+            CharacterInfo mCharacterInfo;
+            if (m_Font.GetCharacterInfo(c, out mCharacterInfo))
+            {
+                nLength++;
+            }
+        }
+        return nLength;
+    }
+
+    private void UpdateMesh()
+    {
+        ResetVertexs();
+        ResetTriangles();
+
+        mMesh.Clear(false);
+        mMesh.SetVertices(vertices);
+        mMesh.SetUVs(0, uvs0);
+        mMesh.SetTriangles(triangles, 0);
+        mMesh.RecalculateBounds();
+
+        mMeshFilter.sharedMesh = mMesh;
+    }
+
+    private void UpdateMaterial()
+    {
+        mMeshRenderer.sharedMaterial = m_Font.material;
+    }
+
+    private void AddVertexs(Vector3 pos, Vector2 uv)
+    {
+        vertices.Add(pos);
+        uvs0.Add(uv);
+    }
+
+
+    private void ResetVertexs()
+    {
+        vertices.Clear();
+        uvs0.Clear();
+
+        float posX = 0f;
+        float fWidth = GetWidth();
+
+        if (mTextAlignment == TextAlignment.Left)
+        {
+            posX = 0f;
+        }
+        else if (mTextAlignment == TextAlignment.Center)
+        {
+            posX = -fWidth / 2f;
+        }
+        else if (mTextAlignment == TextAlignment.Right)
+        {
+            posX = -fWidth;
+        }
+
+        for (int i = 0, nLength = m_Text.Length; i < nLength; i++)
+        {
+            char c = m_Text[i];
+            CharacterInfo mCharacterInfo;
+            if (m_Font.GetCharacterInfo(c, out mCharacterInfo))
+            {
+                Vector2 uvBottomLeft = mCharacterInfo.uvBottomLeft;
+                Vector2 posBottomLeft = new Vector2(mCharacterInfo.minX, mCharacterInfo.minY);
+                posBottomLeft.x += posX;
+                AddVertexs(posBottomLeft, uvBottomLeft);
+
+                Vector2 uvTopLeft = mCharacterInfo.uvTopLeft;
+                Vector2 posuvTopLeft = new Vector2(mCharacterInfo.minX, mCharacterInfo.maxY);
+                posuvTopLeft.x += posX;
+                AddVertexs(posuvTopLeft, uvTopLeft);
+
+
+                Vector2 uvTopRight = mCharacterInfo.uvTopRight;
+                Vector2 posuvTopRight = new Vector2(mCharacterInfo.maxX, mCharacterInfo.maxY);
+                posuvTopRight.x += posX;
+                AddVertexs(posuvTopRight, uvTopRight);
+
+                Vector2 uvBottomRight = mCharacterInfo.uvBottomRight;
+                Vector2 posBottomRight = new Vector2(mCharacterInfo.maxX, mCharacterInfo.minY);
+                posBottomRight.x += posX;
+                AddVertexs(posBottomRight, uvBottomRight);
+
+                posX += mCharacterInfo.advance;
+            }
+        }
+    }
+
+    private void ResetTriangles()
+    {
+        int nOriLength = triangles.Count / 6;
+        int nNowLength = GetValidLength();
+        if (nNowLength > nOriLength)
+        {
+            for(int i = nOriLength; i < nNowLength; i++)
+            {
+                int nBeginIndex = i * 4;
+                triangles.Add(nBeginIndex + 0);
+                triangles.Add(nBeginIndex + 1);
+                triangles.Add(nBeginIndex + 2);
+
+                triangles.Add(nBeginIndex + 2);
+                triangles.Add(nBeginIndex + 3);
+                triangles.Add(nBeginIndex + 0);
+            }
+        }
+        else if (nNowLength < nOriLength)
+        {
+            triangles.RemoveRange(nNowLength, nOriLength - nNowLength);
+        }
+    }
+
+}
---
+++ Project/Assets/TextBeat/Editor/TextMeshProFontEditor.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using TMPro;
+using UnityEngine;
+using System.IO;
+using UnityEngine.TextCore;
+using System.Reflection;
+
+namespace TMPro
+{
+
+    public class TextMeshProFontEditor : Editor
+    {
+        static List<Glyph> m_FontGlyphTable = new List<Glyph>();
+        static List<TMP_Character> m_FontCharacterTable = new List<TMP_Character>();
+
+        [MenuItem("Tools/Create Font")]
+        public static void CreateFont()
+        {
+            Font mFont = Selection.activeObject as Font;
+            if (mFont)
+            {
+                string filePath = AssetDatabase.GetAssetPath(mFont);
+                string tex_DirName = Path.GetDirectoryName(filePath);
+                string tex_FileName = Path.GetFileNameWithoutExtension(filePath);
+                string tex_Path_NoExt = tex_DirName + "/" + tex_FileName;
+
+                TMP_FontAsset fontAsset = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(tex_Path_NoExt + ".asset");
+                if (fontAsset)
+                {
+                    DestroyImmediate(fontAsset, true);
+                }
+
+                TMP_FontAsset mFontAsset = TMP_FontAsset.CreateFontAsset(mFont);
+                AssetDatabase.CreateAsset(mFontAsset, tex_Path_NoExt + ".asset");
+
+                Texture2D oriTex = mFont.material.mainTexture as Texture2D;
+                Texture2D m_FontAtlasTexture = new Texture2D(oriTex.width, oriTex.height, oriTex.format, false);
+                for(int i = 0; i < m_FontAtlasTexture.width; i++)
+                {
+                    for (int j = 0; j < m_FontAtlasTexture.height; j++)
+                    {
+                        m_FontAtlasTexture.SetPixel(i, j, oriTex.GetPixel(i, j));
+                    }
+                }
+
+                m_FontAtlasTexture.Apply();
+
+                m_FontAtlasTexture.name = tex_FileName + " Atlas";
+                mFontAsset.GetType().GetField("m_AtlasWidth", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, m_FontAtlasTexture.width);
+                mFontAsset.GetType().GetField("m_AtlasHeight", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, m_FontAtlasTexture.height);
+                mFontAsset.GetType().GetField("m_AtlasPadding", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, 5);
+                mFontAsset.atlasTextures = new Texture2D[] { (Texture2D)m_FontAtlasTexture };
+
+                Shader default_Shader = Shader.Find("TextMeshPro/Bitmap"); // m_shaderSelection;
+                Material tmp_material = new Material(default_Shader);
+                tmp_material.name = tex_FileName + " Material";
+                tmp_material.SetTexture(ShaderUtilities.ID_MainTex, m_FontAtlasTexture);
+                mFontAsset.material = tmp_material;
+
+                foreach (var v in mFont.characterInfo)
+                {
+                    uint unicode = (uint)v.index;
+                    Glyph mGlyph = GetGlyph(v);
+
+                    m_FontGlyphTable.Add(mGlyph);
+                    m_FontCharacterTable.Add(new TMP_Character(unicode, mGlyph));
+                }
+        
+                mFontAsset.GetType().GetField("m_CharacterTable", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, m_FontCharacterTable);
+                mFontAsset.GetType().GetField("m_GlyphTable", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, m_FontGlyphTable);
+
+                FaceInfo mFaceInfo = GetFaceInfo(mFont);
+                mFontAsset.GetType().GetField("m_FaceInfo", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(mFontAsset, mFaceInfo);
+
+                m_FontAtlasTexture.hideFlags = HideFlags.None;
+                mFontAsset.material.hideFlags = HideFlags.None;
+                
+                AssetDatabase.AddObjectToAsset(m_FontAtlasTexture, mFontAsset);
+                AssetDatabase.AddObjectToAsset(tmp_material, mFontAsset);
+
+                AssetDatabase.SaveAssets();
+
+                mFontAsset.ReadFontAssetDefinition();
+
+                AssetDatabase.Refresh();
+            }
+
+        }
+
+        private static Glyph GetGlyph(CharacterInfo mInfo)
+        {
+            Glyph mGlyph = new Glyph();
+            mGlyph.index = (uint)mInfo.index;
+            mGlyph.scale = 1;
+            mGlyph.glyphRect = new GlyphRect(Rect.MinMaxRect(mInfo.minX, mInfo.minY, mInfo.maxX, mInfo.maxY));
+            mGlyph.atlasIndex = 0;
+
+            GlyphMetrics mGlyphMetrics = new GlyphMetrics(mInfo.glyphWidth, mInfo.glyphHeight, mInfo.bearing, 0, mInfo.advance);
+            mGlyph.metrics = mGlyphMetrics;
+
+            return mGlyph;
+        }
+
+        private static FaceInfo GetFaceInfo(Font mFont)
+        {
+            int minPointSize = 0;
+            int maxPointSize = (int)Mathf.Sqrt((mFont.material.mainTexture.width * mFont.material.mainTexture.height) / mFont.characterInfo.Length) * 3;
+            int m_PointSize = (maxPointSize + minPointSize) / 2;
+
+            FaceInfo mFaceInfo = new FaceInfo();
+            mFaceInfo.familyName = mFont.name;
+            mFaceInfo.styleName = "Regular";
+            mFaceInfo.scale = 1;
+            mFaceInfo.pointSize = m_PointSize;
+            mFaceInfo.lineHeight = mFont.lineHeight;
+            mFaceInfo.ascentLine = mFont.ascent;
+            mFaceInfo.capLine = 0;
+            mFaceInfo.meanLine = 0;
+            mFaceInfo.baseline = 0;
+            return mFaceInfo;
+        }
+
+    }
+}
