e70cd3f370ea038a9c87049544795ea5f5faaee0
xuke <825126369@qq.com>
2025-10-03 14:30:56 +0800

    TODO: GPU 程序化实例，实现类似 ECS 百万级渲染

---
+++ Assets/Work/GPUInstance/GISpriteRendererExample.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GISpriteRendererExample : MonoBehaviour
+{
+    void Start()
+    {
+        MaterialPropertyBlock props = new MaterialPropertyBlock();
+        MeshRenderer renderer;
+
+        foreach (GameObject obj in objects)
+        {
+            float r = Random.Range(0.0f, 1.0f);
+            float g = Random.Range(0.0f, 1.0f);
+            float b = Random.Range(0.0f, 1.0f);
+            props.SetColor("_Color", new Color(r, g, b));
+
+            renderer = obj.GetComponent<MeshRenderer>();
+            renderer.SetPropertyBlock(props);
+        }
+    }
+}
